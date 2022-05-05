3b018d8ffc8d5a2d2d7386037a398bbe4a004029
Luis Rodrigues <luissrodrigues2003@gmail.com>
2022-05-05 09:19:27 -0300

    Criando cabecalho do jogo

---
+++ telaevalidacao.py
@@ -0,0 +1,3 @@
+print("\n\nBem-vindo ao Insper Países \n")
+print("Versão desenvolvida por Luis e Leonardo \n")
+print("Comandos: \n dica       - entra no mercado de dicas \n desisto    - desiste da rodada \n inventario - exibe sua posição \n")
