8aeb83f8dff0967f760cda2d7338753804e657fe
Luis Rodrigues <luissrodrigues2003@gmail.com>
2022-05-05 09:26:27 -0300

    corrigindo banco de funcoes

--- telaevalidacao.py
+++ telaevalidacao.py
@@ -1,3 +1,11 @@
+from dados import DADOS
+from funcoes import sorteia_pais
+
+
 print("\n\nBem-vindo ao Insper Países \n")
 print("Versão desenvolvida por Luis e Leonardo \n")
 print("Comandos: \n dica       - entra no mercado de dicas \n desisto    - desiste da rodada \n inventario - exibe sua posição \n")
+
+#Sorteando um pais:
+pais = sorteia_pais(DADOS)
+print(pais)
