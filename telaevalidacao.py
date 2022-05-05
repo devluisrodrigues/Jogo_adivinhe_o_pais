d65da5f5749f42ef87302c58805b8c74162b0cba
Luis Rodrigues <luissrodrigues2003@gmail.com>
2022-05-05 09:29:55 -0300

    Dando inicio ao jogo

--- telaevalidacao.py
+++ telaevalidacao.py
@@ -1,5 +1,6 @@
+from email.mime import base
 from dados import DADOS
-from funcoes import sorteia_pais
+from funcoes import normaliza, sorteia_pais
 
 
 print("\n\nBem-vindo ao Insper Países \n")
@@ -7,5 +8,7 @@ print("Versão desenvolvida por Luis e Leonardo \n")
 print("Comandos: \n dica       - entra no mercado de dicas \n desisto    - desiste da rodada \n inventario - exibe sua posição \n")
 
 #Sorteando um pais:
-pais = sorteia_pais(DADOS)
+basenormal = normaliza(DADOS)
+pais = sorteia_pais(basenormal)
+print("Um país foi escolhido, tente adivinhar!")
 print(pais)
